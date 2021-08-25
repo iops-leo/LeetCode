fb1b22ed34488cef20dbb88894d4c88ad6acc1a9
Hyungchul Shin <50747934+leaflife84@users.noreply.github.com>
2021-08-26 00:03:56 +0900

    Time: 9 ms (30.03%), Space: 39.4 MB (39.83%) - LeetHub

---
+++ valid-anagram/valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+ public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            Character c = s.charAt(i);
+            map1.put(c,map1.getOrDefault(c,0)+1);
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            Character c = t.charAt(i);
+            map2.put(c,map2.getOrDefault(c,0)+1);
+        }
+
+        if(map1.size() != map2.size()){
+            return false;
+        }
+        for(Character c : map1.keySet()){
+            if(map2.get(c) == null){
+                return false;
+            }else{
+                if(!map1.get(c).equals(map2.get(c))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
